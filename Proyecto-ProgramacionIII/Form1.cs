aa4592d8ab5a90b2463fd9731521049a68966394
elias-escritorio <eliasvroca@gmail.com>
2024-06-26 13:19:08 -0400

    add: Se agregó conexion entre el formulario principal y los formularios Cadena

--- Proyecto-ProgramacionIII/Form1.cs
+++ Proyecto-ProgramacionIII/Form1.cs
@@ -1,3 +1,6 @@
+
+using Proyecto_ProgramacionIII.Cadena;
+
 namespace Proyecto_ProgramacionIII
 {
     public partial class Form1 : Form
@@ -9,27 +12,32 @@ namespace Proyecto_ProgramacionIII
 
         private void ejercicio1CadenaToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            Cadena1 cadena1 = new Cadena1();
+            cadena1.Show();
         }
 
         private void ejercicio2CadenaToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            Cadena2 cadena2 = new Cadena2();
+            cadena2.Show();
         }
 
         private void ejercicio3CadenaToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            Cadena3 cadena3 = new Cadena3();
+            cadena3.Show();
         }
 
         private void ejercicio4CadenaToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            Cadena4 cadena4 = new Cadena4();
+            cadena4.Show();
         }
 
         private void ejercicio5CadenaToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            Cadena5 cadena5 = new Cadena5();
+            cadena5.Show();
         }
     }
 }
