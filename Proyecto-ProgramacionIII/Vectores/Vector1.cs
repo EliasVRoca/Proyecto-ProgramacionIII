ac4ad9758e124f93a41c7c9c28f6a002d52ceb42
elias-escritorio <eliasvroca@gmail.com>
2024-06-26 15:25:06 -0400

    add: formularios vectores

---
+++ Proyecto-ProgramacionIII/Vectores/Vector1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_ProgramacionIII.Vectores
+{
+    public partial class Vector1 : Form
+    {
+        public Vector1()
+        {
+            InitializeComponent();
+        }
+    }
+}
