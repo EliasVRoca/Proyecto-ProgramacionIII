a97c4ec5d524e426a79c6cbd965ef4d74c0e6023
elias-escritorio <eliasvroca@gmail.com>
2024-06-26 13:16:20 -0400

    add: Se agregó los formularios para Cadena

---
+++ Proyecto-ProgramacionIII/Cadena/Cadena3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_ProgramacionIII.Cadena
+{
+    public partial class Cadena3 : Form
+    {
+        public Cadena3()
+        {
+            InitializeComponent();
+        }
+    }
+}
